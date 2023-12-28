663e38535caeb24ae034c7639675c58c04a255e5
chenineazzeddine <azzeddine.chenine.pro@gmail.com>
2023-12-28 17:21:56 +0100

    feat: debug

--- ds/loader.py
+++ ds/loader.py
@@ -33,7 +33,7 @@ class DataLoader:
         # from ds.utils import decode_tf_record_example
 
         file_ds = tf.data.Dataset.list_files(f"{self.directory}/{self.split}*.tfrecord")
-        print(file_ds)
+        print(file_ds, f"{self.directory}/{self.split}*.tfrecord")
 
         file_ds = file_ds.shard(num_shards=self.num_shards, index=self.shard)
 
