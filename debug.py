482e78ff8011845353beeedcee96b29800ada4da
chenineazzeddine <azzeddine.chenine.pro@gmail.com>
2023-12-28 15:59:14 +0100

    feat: debug script

---
+++ debug.py
@@ -0,0 +1,34 @@
+import jax
+import tyro
+
+from config import Config, get_default_config
+from training.trainer import Trainer
+
+if jax.process_index() == 0:
+    print(
+        f"TPU pod initialized, {jax.process_count()} host/s, {jax.local_device_count()} core per host, {jax.device_count()} total"
+    )
+
+# ============= Init configs ============= #
+
+config = tyro.cli(Config, default=get_default_config())
+
+# ============= Init Random keys loaders ============= #
+
+key = jax.random.PRNGKey(0)
+data_rng_key, training_key, key = jax.random.split(key, 3)
+
+# ============= Init training state ============= #
+
+trainer = Trainer(config=config)
+
+start_iter = 0
+best_valid_loss = 1e6
+if config.restore == "scratch":
+    train_state = trainer.make_train_state()
+elif config.restore == "pre-trained":
+    train_state, best_valid_loss = trainer.restore()
+    start_iter = train_state.step + 1
+elif config.restore == "gpt-2":
+    train_state = trainer.restore_openai_gpt()
+    raise ValueError(f"unknown restore method {config.restore}")
