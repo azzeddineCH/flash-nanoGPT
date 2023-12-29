8288b209810b9dec63ab0916bba78635f31ead32
chenineazzeddine <azzeddine.chenine.pro@gmail.com>
2023-12-29 02:36:10 +0100

    debug

--- train.py
+++ train.py
@@ -1,5 +1,4 @@
 import dataclasses
-import multiprocessing
 import time
 
 import jax
@@ -7,9 +6,7 @@ import tyro
 
 import wandb
 from config import Config, get_default_config
-from ds.loader import DataLoader
 from training.trainer import Trainer
-from training.utils import TrainMetrics
 
 if jax.process_index() == 0:
     print(
@@ -53,91 +50,93 @@ elif config.restore == "gpt-2":
 if jax.process_index() == 0:
     print("Loading dataset ...")
 
-train_data_iter = DataLoader(
-    directory=config.dataset_dir,
-    batch_size=config.batch_size // jax.process_count(),
-    block_size=config.block_size,
-    split="train",
-    prefetch=config.prefetch,
-    buffer_size=config.buffer_size,
-    num_shards=jax.process_count(),
-    shard=jax.process_index(),
-    num_workers=multiprocessing.cpu_count() // 2,
-).get_iterator()
+time.sleep(5)
 
-validation_data_iter = DataLoader(
-    directory=config.dataset_dir,
-    batch_size=config.batch_size // jax.process_count(),
-    block_size=config.block_size,
-    split="val",
-    prefetch=config.prefetch,
-    buffer_size=config.buffer_size,
-    num_shards=jax.process_count(),
-    shard=jax.process_index(),
-    num_workers=multiprocessing.cpu_count() // 4,
-).get_iterator()
-
-# ============= Training Loop ============= #
-
-for i in range(start_iter, config.num_iters):
-    # ============= Training ============= #
-
-    t0 = time.time()
-    train_batch_key, train_step_key, training_key = jax.random.split(training_key, 3)
-    train_state, train_metrics = trainer.training_step(
-        train_step_key, train_state, batch=next(train_data_iter)
-    )
-    step_time_s = time.time() - t0
-
-    # ============= Evaluation ============= #
-
-    if train_state.step == 1 or train_state.step % config.eval_freq == 0:
-        valid_loss = train_loss = 0
-        train_eval_key, valid_eval_key, training_key = jax.random.split(training_key, 3)
-        for j in range(config.eval_num_steps):
-            valid_loss += (
-                trainer.validation_step(
-                    train_step_key, train_state, batch=next(validation_data_iter)
-                )
-                / config.eval_num_steps
-            )
-
-            train_loss += (
-                trainer.validation_step(
-                    train_step_key, train_state, batch=next(train_data_iter)
-                )
-                / config.eval_num_steps
-            )
-
-        if config.wandb and jax.process_index() == 0:
-            wandb.log(
-                {
-                    "iter": train_state.step,
-                    "train/loss": train_loss,
-                    "val/loss": valid_loss,
-                    "lr": train_state.lr,
-                    "loss_scale": train_state.loss_scale.loss_scale,
-                    "grads_gnorm": train_metrics.grads_gnorm,
-                    "params_gnorm": train_metrics.params_gnorm,
-                    "time_ms": step_time_s * 1000,
-                }
-            )
-
-        if valid_loss < best_valid_loss:
-            best_valid_loss = valid_loss
-            if jax.process_index() == 0:
-                print(
-                    f"iter: {train_state.step} |  val loss {valid_loss} | train loss {train_loss}"
-                )
-                if config.save_checkpoint:
-                    trainer.save(train_state, metrics=TrainMetrics(loss=valid_loss))
-
-    # ============= Logging ============= #
-
-    if train_state.step % config.log_freq == 0 and jax.process_index() == 0:
-        print(
-            f"iter: {train_state.step} | loss: {train_metrics.loss} | time_ms: {step_time_s * 1000}"
-        )
-
-if config.wandb and jax.process_index() == 0:
-    wandb.finish()
+# train_data_iter = DataLoader(
+#     directory=config.dataset_dir,
+#     batch_size=config.batch_size // jax.process_count(),
+#     block_size=config.block_size,
+#     split="train",
+#     prefetch=config.prefetch,
+#     buffer_size=config.buffer_size,
+#     num_shards=jax.process_count(),
+#     shard=jax.process_index(),
+#     num_workers=multiprocessing.cpu_count() // 2,
+# ).get_iterator()
+#
+# validation_data_iter = DataLoader(
+#     directory=config.dataset_dir,
+#     batch_size=config.batch_size // jax.process_count(),
+#     block_size=config.block_size,
+#     split="val",
+#     prefetch=config.prefetch,
+#     buffer_size=config.buffer_size,
+#     num_shards=jax.process_count(),
+#     shard=jax.process_index(),
+#     num_workers=multiprocessing.cpu_count() // 4,
+# ).get_iterator()
+#
+# # ============= Training Loop ============= #
+#
+# for i in range(start_iter, config.num_iters):
+#     # ============= Training ============= #
+#
+#     t0 = time.time()
+#     train_batch_key, train_step_key, training_key = jax.random.split(training_key, 3)
+#     train_state, train_metrics = trainer.training_step(
+#         train_step_key, train_state, batch=next(train_data_iter)
+#     )
+#     step_time_s = time.time() - t0
+#
+#     # ============= Evaluation ============= #
+#
+#     if train_state.step == 1 or train_state.step % config.eval_freq == 0:
+#         valid_loss = train_loss = 0
+#         train_eval_key, valid_eval_key, training_key = jax.random.split(training_key, 3)
+#         for j in range(config.eval_num_steps):
+#             valid_loss += (
+#                 trainer.validation_step(
+#                     train_step_key, train_state, batch=next(validation_data_iter)
+#                 )
+#                 / config.eval_num_steps
+#             )
+#
+#             train_loss += (
+#                 trainer.validation_step(
+#                     train_step_key, train_state, batch=next(train_data_iter)
+#                 )
+#                 / config.eval_num_steps
+#             )
+#
+#         if config.wandb and jax.process_index() == 0:
+#             wandb.log(
+#                 {
+#                     "iter": train_state.step,
+#                     "train/loss": train_loss,
+#                     "val/loss": valid_loss,
+#                     "lr": train_state.lr,
+#                     "loss_scale": train_state.loss_scale.loss_scale,
+#                     "grads_gnorm": train_metrics.grads_gnorm,
+#                     "params_gnorm": train_metrics.params_gnorm,
+#                     "time_ms": step_time_s * 1000,
+#                 }
+#             )
+#
+#         if valid_loss < best_valid_loss:
+#             best_valid_loss = valid_loss
+#             if jax.process_index() == 0:
+#                 print(
+#                     f"iter: {train_state.step} |  val loss {valid_loss} | train loss {train_loss}"
+#                 )
+#                 if config.save_checkpoint:
+#                     trainer.save(train_state, metrics=TrainMetrics(loss=valid_loss))
+#
+#     # ============= Logging ============= #
+#
+#     if train_state.step % config.log_freq == 0 and jax.process_index() == 0:
+#         print(
+#             f"iter: {train_state.step} | loss: {train_metrics.loss} | time_ms: {step_time_s * 1000}"
+#         )
+#
+# if config.wandb and jax.process_index() == 0:
+#     wandb.finish()
