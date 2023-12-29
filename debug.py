bfaef6acdca5838d36a0dab62555155eecbd6a23
chenineazzeddine <azzeddine.chenine.pro@gmail.com>
2023-12-29 23:43:40 +0100

    debug

--- debug.py
+++ debug.py
@@ -1,5 +1,4 @@
 import dataclasses
-import time
 
 import jax
 import tyro
@@ -7,8 +6,6 @@ import tyro
 import wandb
 from config import Config, get_default_config
 from ds.loader import DataLoader
-from training.trainer import Trainer
-from training.utils import TrainMetrics
 
 
 def log(tree):
@@ -40,21 +37,6 @@ key = jax.random.PRNGKey(config.seed)
 state_key, training_key, key = jax.random.split(key, 3)
 training_key = jax.random.fold_in(training_key, jax.process_index())
 
-# ============= Init training state ============= #
-
-trainer = Trainer(config=config)
-
-start_iter = 0
-best_valid_loss = 1e6
-if config.restore == "scratch":
-    train_state = trainer.make_train_state(state_key)
-elif config.restore == "pre-trained":
-    train_state, best_valid_loss = trainer.restore()
-    start_iter = train_state.step + 1
-elif config.restore == "gpt-2":
-    train_state = trainer.restore_openai_gpt()
-    raise ValueError(f"unknown restore method {config.restore}")
-
 # ============= Init ds loaders ============= #
 if jax.process_index() == 0:
     print("Loading dataset ...")
@@ -70,77 +52,10 @@ train_data_iter = DataLoader(
     shard=jax.process_index(),
 ).get_iterator()
 
-validation_data_iter = DataLoader(
-    directory=config.dataset_dir,
-    batch_size=config.batch_size // jax.process_count(),
-    block_size=config.block_size,
-    split="val",
-    prefetch=config.prefetch,
-    buffer_size=config.buffer_size,
-    num_shards=8,
-    shard=jax.process_index() % 8,
-).get_iterator()
-
 # ============= Training Loop ============= #
 
-for i in range(start_iter, config.num_iters):
+for i in range(0, config.num_iters):
     # ============= Training ============= #
 
-    t0 = time.time()
     train_batch = next(train_data_iter)
-    step_key, training_key = jax.random.split(training_key, 2)
-    train_state, train_metrics = trainer.training_step(
-        step_key, train_state, train_batch
-    )
-    step_time_s = time.time() - t0
-
-    # ============= Evaluation ============= #
-    if train_state.step == 1 or train_state.step % config.eval_freq == 0:
-        valid_loss = train_loss = 0
-        train_eval_key, valid_eval_key, training_key = jax.random.split(training_key, 3)
-        for j in range(config.eval_num_steps):
-            valid_batch = next(validation_data_iter)
-            train_batch = next(train_data_iter)
-            valid_loss += (
-                trainer.validation_step(valid_eval_key, train_state, valid_batch)
-                / config.eval_num_steps
-            )
-            train_loss += (
-                trainer.validation_step(train_eval_key, train_state, train_batch)
-                / config.eval_num_steps
-            )
-
-        if valid_loss < best_valid_loss:
-            best_valid_loss = valid_loss
-            if jax.process_index() == 0:
-                print(
-                    f"iter: {train_state.step} |  val loss {valid_loss} | train loss {train_loss}"
-                )
-                if config.save_checkpoint:
-                    trainer.save(train_state, metrics=TrainMetrics(loss=valid_loss))
-
-        if config.wandb and jax.process_index() == 0:
-            logs = {
-                "iter": train_state.step,
-                "train/loss": train_loss,
-                "val/loss": valid_loss,
-                "lr": train_state.lr,
-                "loss_scale": train_state.loss_scale.loss_scale,
-                "grads_gnorm": train_metrics.grads_gnorm,
-                "params_gnorm": train_metrics.params_gnorm,
-            }
-
-            if train_state.step > 1:
-                # ignore compilation time
-                logs["time_ms"] = step_time_s * 1000
-
-            log(logs)
-    # ============= Logging ============= #
-
-    if train_state.step % config.log_freq == 0 and jax.process_index() == 0:
-        print(
-            f"iter: {train_state.step} | loss: {train_metrics.loss} | time_ms: {step_time_s * 1000}"
-        )
-
-if config.wandb and jax.process_index() == 0:
-    wandb.finish()
+    wandb.log({"iter": i})
