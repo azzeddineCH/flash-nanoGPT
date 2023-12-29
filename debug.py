41d56a090bc61aac0ba36b36d2ba592f21445eef
chenineazzeddine <azzeddine.chenine.pro@gmail.com>
2023-12-29 15:35:31 +0100

    debug

--- debug.py
+++ debug.py
@@ -1,40 +1,18 @@
-import dataclasses
-import multiprocessing
-
 import jax
 import tyro
 
-import wandb
 from config import Config, get_default_config
-from ds.loader import DataLoader
+from ds.utils import Batch
 from training.trainer import Trainer
-
-if jax.process_index() == 0:
-    print(
-        f"TPU pod initialized, {jax.process_count()} host/s, {jax.local_device_count()} core per host, {jax.device_count()} total"
-    )
 
 # ============= Init configs ============= #
 
 config = tyro.cli(Config, default=get_default_config())
 
-# ============= Init Logging ============= #
-
-if config.wandb and jax.process_index() == 0:
-    wandb.init(
-        project=config.wandb_project_name,
-        name=config.wandb_run_id,
-        config=dataclasses.asdict(config),
-    )
-
 # ============= Init Random keys loaders ============= #
 
 key = jax.random.PRNGKey(0)
 data_rng_key, training_key, key = jax.random.split(key, 3)
-
-# ============= Init ds loaders ============= #
-if jax.process_index() == 0:
-    print("Loading dataset ...")
 
 # ============= Init training state ============= #
 
@@ -51,20 +29,17 @@ elif config.restore == "gpt-2":
     train_state = trainer.restore_openai_gpt()
     raise ValueError(f"unknown restore method {config.restore}")
 
-# ============= Init ds loaders ============= #
+# ============= Training Loop ============= #
 
-train_data_iter = DataLoader(
-    directory=config.dataset_dir,
-    batch_size=config.batch_size // jax.process_count(),
-    block_size=config.block_size,
-    split="train",
-    prefetch=config.prefetch,
-    buffer_size=config.buffer_size,
-    num_shards=jax.process_count(),
-    shard=jax.process_index(),
-    num_workers=multiprocessing.cpu_count() // 2,
-).get_iterator()
+random_input = jax.random.randint(
+    key, (config.batch_size, config.block_size), minval=0, maxval=config.vocab_size
+)
+dummy_batch = Batch(
+    inputs=random_input,
+    labels=random_input,
+)
 
-print(next(train_data_iter).inputs.shape, "<-----", jax.process_index())
-print(next(train_data_iter).inputs.shape, "<-----", jax.process_index())
-print(next(train_data_iter).inputs.shape, "<-----", jax.process_index())
+train_batch_key, train_step_key, training_key = jax.random.split(training_key, 3)
+train_state, train_metrics = trainer.training_step(
+    train_step_key, train_state, batch=dummy_batch
+)
